69d730aca726f6a888ebe82560d679a1ee12705b
Yahia <yahiaelafia1@gmail.com>
2026-04-07 10:13:19 +0100

    PHP

--- Chapiteres/Chapitre 11 – Interaction simple avec la base de données via PDO/test_pdo.php
+++ Chapiteres/Chapitre 11 – Interaction simple avec la base de données via PDO/test_pdo.php
@@ -1,1 +1,50 @@
-en cours ....
+<?php
+
+require 'database.php';
+
+$db = (new Database())->getConnection();
+
+$stmt = $db->query("SELECT * from posts");
+
+$posts =  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "Post_id : " . $post['id_post'] . "<br>" . "Title : " . $post['title'] . "<br>" . "Id_user : " . $post['id_user'] . "<br>" . "Publish_date : " . $post['publish_date'] ."<br>" . "USER : " . $post['id_user'] . "<br>" . "Category : " . $post['id_category'] .   "<br><br>";
+}
+
+
+echo "<br>";
+
+
+echo "<br>";
+echo "<br>";
+
+
+$sql = "INSERT INTO posts (title, content, publish_date, statu, id_user, id_category) 
+        VALUES (:title, :content, :publish_date, :statu, :id_user, :id_category)";
+        
+$stmt = $db->prepare($sql);
+
+$stmt->execute([
+    ':title'        => 'My First Post',
+        ':content'      => 'This is a test content.',
+        ':publish_date' => date('Y-m-d H:i:s'),
+        ':statu'        => 'published',
+        ':id_category'  => 1,
+        ':id_user'      => 1, 
+]);
+
+
+
+echo "<br>";
+echo "<br>";
+
+
+$stmt = $db->prepare("UPDATE posts SET title = :title WHERE id_post = :id_post");
+$stmt->execute(['title' => 'Zoubir', 'id_post' => 1]);
+
+
+$stmt = $db->prepare("DELETE FROM posts WHERE id_post = :post");
+$stmt->execute([
+    'post' => 1
+]);
--- Prototype/articles.php
+++ Prototype/articles.php
@@ -3,7 +3,7 @@ require_once 'database.php';
 class Article {
     public static function readAll(){
         $db = new Database();
-        $conn = $db->getconnection();
+        $conn = $db->getConnection();
         $sql = "SELECT auteur,title,description,date_pub FROM article ";
         $stmt = $conn->query($sql);  
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
